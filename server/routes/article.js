7f2828ba2370974ece9be28afa17f3ce7ad0e30c
anggraito <anggraito.laras@gmail.com>
2017-10-12 14:34:08 +0700

    update article success

--- server/routes/article.js
+++ server/routes/article.js
@@ -6,5 +6,6 @@ const auth = require('../helpers/auth')
 router.get('/', setArticle.findAllArticle)
 router.post('/', auth.isUser, setArticle.createArticle)
 router.get('/:id', setArticle.getArticle)
+router.put('/:id', auth.isUser, setArticle.getArticle)
 
 module.exports = router
